98d7138101b05fd0dcceee453772001445e3417b
jzpacheco <jeffersonpacheco@hotmail.com>
2023-10-23 23:28:27 -0300

    Creating Modal to display detailed shopping list

--- app/app.component.js
+++ app/app.component.js
@@ -4,5 +4,15 @@ let shoppingListApp = angular.module("shoppingList", []);
 shoppingListApp.controller("shoppingListCtrl", function($http, $scope) {
     $http.get('http://localhost:8080/api/shopList/v1').then(function (response){
         $scope.myData = response.data;
-    })
+    });
+
+    $scope.openModal = function() {
+        $scope.nome = "";
+        $scope.idade = "";
+
+        
+        // Abra o modal Bootstrap
+        $('#myModal').modal('show');
+    };
+
 })
